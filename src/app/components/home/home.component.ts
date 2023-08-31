657693d60ccefa06a795f149352fba1c5d9b908b
angelo lorensi <angelomozza@gmail.com>
2023-08-31 16:42:02 -0300

    home header and tweet box adjustments

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -9,6 +9,11 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
+  //header style variables
+  fyHeader: string = 'underline #1672b0';
+  followingHeader : string = '';
+
+  //data variables
   token:string | null = '';
   username:string | null  = '';
   nickname:string | null = '';
@@ -20,29 +25,30 @@ export class HomeComponent implements OnInit {
     private router:Router,
     private authService:AuthenticationService
   ) {
-
   }
 
   ngOnInit(): void {
+    //check if logged in
     this.token = localStorage.getItem('token');
     this.authService.userLoggedIn(this.token).subscribe(
       (data) =>{
         console.log(data);
-        localStorage.setItem('id', data.id)
-        localStorage.setItem('username', data.username)
-        localStorage.setItem('nickname', data.nickname)
-        localStorage.setItem('email', data.email)
-        localStorage.setItem('dob', data.dateOfBirth)
-        localStorage.setItem('profilePicture', data.profilePicture)
-        localStorage.setItem('bannerPicture', data.bannerPicture)
-        localStorage.setItem('enabled', data.enabled)
-        localStorage.setItem('phone', data.phone)
+        localStorage.setItem('id', data.id);
+        localStorage.setItem('username', data.username);
+        localStorage.setItem('nickname', data.nickname);
+        localStorage.setItem('email', data.email);
+        localStorage.setItem('dob', data.dateOfBirth);
+        localStorage.setItem('profilePicture', data.profilePicture);
+        localStorage.setItem('bannerPicture', data.bannerPicture);
+        localStorage.setItem('enabled', data.enabled);
+        localStorage.setItem('phone', data.phone);
       },
       (error) => {
         this.router.navigateByUrl('/login');
       }
     );
 
+    //set data to the page
     this.username = localStorage.getItem('username');
     this.nickname = localStorage.getItem('nickname');
     if(this.nickname == 'null'){
@@ -50,6 +56,17 @@ export class HomeComponent implements OnInit {
     }
     this.email = localStorage.getItem('email');
     this.phone = localStorage.getItem('phone');
+  }
+
+  getSelectedHeader(header: string){
+    if(header == 'For You'){
+      this.fyHeader = 'underline #1672b0';
+      this.followingHeader = '';
+    }
+    if(header == 'Following'){
+      this.followingHeader = 'underline #1672b0';
+      this.fyHeader = '';
+    }
   }
 
 }
